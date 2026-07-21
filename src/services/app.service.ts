0f23530749f368d248744d584fc65752d62945aa
syed-huzaifa-ali <syedhuzaifaali366@gmail.com>
2026-07-21 18:32:38 +0500

    minor

--- src/services/app.service.ts
+++ src/services/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'tiktok-developers-site-verification=cZVVriPlLtaFAnqPFRc13Tx77DyZBbqQ';
   }
 }
